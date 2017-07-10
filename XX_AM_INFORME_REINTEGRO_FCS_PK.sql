21bdbf13e739b454be31555e4f8b607003969100
Braian1990 <braian.christiansen@ua.com.ar>
2017-07-10 09:20:58 -0300

    Reintegros
    Commit 1

---
+++ XX_AM_INFORME_REINTEGRO_FCS_PK.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE PACKAGE APPS.XX_AM_INFORME_REINTEGRO_FC_PK AUTHID CURRENT_USER AS
+/*=========================================================================+
+|                                                                          |
+| FILENAME                                                                 |
+|    XX_AM_INFORME_REINTEGRO_FCS_PK.sql                                    |
+|                                                                          |
+| DESCRIPTION                                                              |
+|    Crea el paquete XX_AM_INFORME_REINTEGRO_FCS_PK                        |
+|    que contiene funciones y procedimientos para el generar el            |
+|    informe de reintegros y creacion de facturas.                         |
+|                                                                          |
+| LANGUAGE                                                                 |
+|    PL/SQL                                                                |
+|                                                                          |
+| HISTORY                                                                  |
+|    23/03/2017         bchristiansen             Created                  |
+|                                                                          |
+| NOTES                                                                    |
+|                                                                          |
++=========================================================================*/
+/* XX_AM_INFORME_REINTEGRO_FCS_PK.sql 1.0 23/03/2017 */
+
+/*=========================================================================+
+|                                                                          |
+| Public Procedure                                                         |
+|    main                                                                  |
+|                                                                          |
+| Description                                                              |
+|    Procedimiento que muestra el informe de lo procesado en el proceso    |
+|    de reintegros y creacion de facturas.                                 |
+|                                                                          |
+| Parameters                                                               |
+|                                                                          |
++=========================================================================*/
+PROCEDURE main (x_errbuf               OUT     VARCHAR2
+               ,x_retcode              OUT     VARCHAR2
+               ,p_request_id            IN     NUMBER
+			   ,p_vendor_id             IN     NUMBER
+                );
+
+END XX_AM_INFORME_REINTEGRO_FC_PK;
+/
